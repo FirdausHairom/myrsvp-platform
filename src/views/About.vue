8d1e45c0660ebcdcbcc064ae9e19b6f7d7d76363
FirdausHairom <firdaushairom@gmail.com>
2024-06-09 10:17:24 +0800

    add router

---
+++ src/views/About.vue
@@ -0,0 +1,14 @@
+<!-- src/views/About.vue -->
+<template>
+    <div>
+      <h1>About</h1>
+      <p>This is the About page.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'AboutPage',
+  };
+  </script>
+  
---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<!-- src/views/Home.vue -->
+<template>
+    <div>
+      <h1>Home</h1>
+      <p>Welcome to the Home page!</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HomePage',
+  };
+  </script>
+  
